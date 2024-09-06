dc12face4c9039c5cc6e832fcfab4548baae982b
Baruwa Baruwa <aladebaruwa@gmail.com>
2024-09-06 14:02:47 +0000

    Added `response` module, refactored `HttpRequest` and `HttpHeader` structs, implemented `Version` and `Method` enums, and updated `main.rs` to handle HTTP requests and responses.

--- src/http/request.rs
+++ src/http/request.rs
@@ -1,21 +1,66 @@
+use std::{collections::HashMap, fmt::{write, Display}, hash::Hash, io, result, str::FromStr};
+
+use super::response::HttpResponse;
+
 #[derive(Debug)]
 pub struct HttpRequest {
     method: Method,
-    route: Route,  // Ensure Route is defined somewhere
+    pub resource: Resource,
     version: Version,
     headers: HttpHeader,
-    request_body: String
+    pub request_body: String,
 }
+
+impl HttpRequest {
+    pub fn response(&self) -> io::Result<HttpResponse> {
+        HttpResponse::new(self)
+    }
+    pub fn new(request: &str) -> io::Result<HttpRequest> {
+        let method: Method = Method::new(request);
+        let resource: Resource = if let Some(resource) = Resource::new(request) {
+            resource
+        } else {
+            Resource {
+                path: "".to_string(),
+            }
+        };
+        let version: Version = Version::new(request)
+            .map_err(|err| io::Error::new(io::ErrorKind::InvalidData, err.msg))?;
+
+        let headers: HttpHeader = if let Some(headers) = HttpHeader::new(request) {
+            headers
+        } else {
+            HttpHeader {
+                headers: HashMap::new(),
+            }
+        };
+
+        let request_body: String = if let Some((_header, body)) = request.split_once("\r\n\r\n") {
+            body.to_string()
+        } else {
+            String::new()
+        };
+
+        Ok(HttpRequest {
+            method,
+            resource,
+            version,
+            headers,
+            request_body,
+        })
+    }
+}
+
 
 #[derive(Debug)]
 struct HttpHeader {
-    headers: HashMap<String, String>
+    headers: HashMap<String, String>,
 }
 
 impl HttpHeader {
     pub fn new(request: &str) -> Option<HttpHeader> {
-        let httpheader = HttpHeader {
-            headers: HashMap::new()
+        let mut httpheader = HttpHeader {
+            headers: HashMap::new(),
         };
         let (_, header_str) = request.split_once("\r\n")?;
 
@@ -23,57 +68,63 @@ impl HttpHeader {
             if line.is_empty() {
                 break;
             }
-            let (header, value) = line.split_once(":") ?;
+            let (header, value) = line.split_once(":")?;
             httpheader
                 .headers
                 .insert(header.trim().to_string(), value.trim().to_string());
         }
-
         Some(httpheader)
     }
 }
 
 #[derive(Debug)]
-enum Version {
+pub enum Version {
     V1_1,
     V2_0,
 }
 
-#[derive(Debug)]
-struct VersionError {
-    msg: String
+impl Display for Version {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let msg = match self {
+            Version::V1_1 => "HTTP/1.1",
+            Version::V2_0 => "HTTP/2",
+        };
+        write!(f, "{}", msg)
+    }
+}
+pub struct VersionError {
+    msg: String,
 }
 
 impl Display for VersionError {
-    fn fmt(&self, f: &mut std::fmt::formatter<'_>) -> std::fmt::Result {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(f, "{}", self.msg)
     }
 }
 
 impl Version {
-    pub new()
+    pub fn new(request: &str) -> Result<Self, VersionError> {
+        Version::from_str(request)
+    }
 }
-
 impl FromStr for Version {
     type Err = VersionError;
 
     fn from_str(request: &str) -> Result<Self, Self::Err> {
-        let request_split  = request.split_once("\r\n");
-        if let Some((method_line, _rest)) = request {
-            let method_line = method_line.split_ascii_whitespace();
-            for line in method_line
-            {
-                if line = "HTTP/1.1" {
-                    return Ok(Version::V1_1)
-                } else if line == "HTTP/2" || line == "HTTP/2.0" {
+        let request_split = request.split_once("\r\n");
+        if let Some((metho_line, _rest)) = request_split {
+            let splits = metho_line.split_ascii_whitespace();
+            for split in splits {
+                if split == "HTTP/1.1" {
+                    return Ok(Version::V1_1);
+                } else if split == "HTTP/2" || split == "HTTP/2.0" {
                     return Ok(Version::V2_0);
                 };
-            };
+            }
         };
+
         let invalid = format!("Unknown protocol version in {}", request);
-        let version_error = VersionError {
-            msg:invalid
-        };
+        let version_error = VersionError { msg: invalid };
         Err(version_error)
     }
 }
@@ -82,10 +133,54 @@ impl FromStr for Version {
 enum Method {
     Get,
     Post,
-    Uninitialized
+    Uninitialised,
+}
+
+impl Method {
+    pub fn new(request: &str) -> Method {
+        let request_split = request.split_once("\r\n");
+        if let Some((method_line, _rest)) = request_split {
+            let method_line: Option<(&str, &str)> = method_line.split_once(' ');
+            if let Some((method, _rest)) = method_line {
+                return match method {
+                    "GET" => Method::Get,
+                    "POST" => Method::Post,
+                    _ => Method::Uninitialised,
+                };
+            };
+        };
+        Method::Uninitialised
+    }
+    pub fn identify(s: &str) -> Method {
+        match s {
+            "GET" => Method::Get,
+            "POST" => Method::Post,
+            _ => Method::Uninitialised,
+        }
+    }
 }
 
 #[derive(Debug)]
-struct Route {
-    path: String,
+pub struct Resource {
+   pub path: String,
+}
+
+impl Resource {
+    pub fn new(request: &str) -> Option<Resource> {
+        if let Some((request_method, _)) = request.split_once("\r\n") {
+            let (method, rest) = request_method.split_once(' ')?;
+            return match Method::identify(method) {
+                Method::Get | Method::Post => {
+                    let (resource, _protocol_version) = rest.split_once(' ')?;
+                    let resource = resource.trim();
+                    let resource = resource.trim_start_matches('/');
+                    return Some(Resource {
+                        path: resource.to_string(),
+                    });
+                }
+                Method::Uninitialised => None,
+            };
+        };
+        None
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,49 +1,48 @@
 use std::{
-    io::{self, Write, Read},
-    net::{
-        TcpListener,
-        TcpStream,
-        SocketAddr,
-        Ipv4Addr
-    }
+    io::{self, Read, Write},
+    net::{Ipv4Addr, SocketAddr, TcpListener, TcpStream},
 };
 
+use simple_http::http::request;
+
 fn create_socket() -> SocketAddr {
-    SocketAddr::new(std::net::IpAddr::V4(std::net::Ipv4Addr::LOCALHOST), 5500)
+    SocketAddr::new(std::net::IpAddr::V4(Ipv4Addr::LOCALHOST), 5500)
 }
 
 fn handle_client(stream: &mut TcpStream) -> io::Result<()> {
     let mut buffer = [0; 1024];
     stream.read(&mut buffer)?;
 
-    let valid_response = "HTTP/2 200\ncontent-type: text/html\nvary: Accept-Encoding\r\n\r\n
-    <html>
-    <body>
-    <h1>Hello World!</h1>
-    </body>
-    </html>
-    ";
+    let buf_str = String::from_utf8_lossy(&buffer);
+    let request= request::HttpRequest::new(&buf_str)?;
+    let response = request.response()?;
 
-    stream.write(&mut valid_response.as_bytes())?;
+    println!("{:?}", &response);
+    println!("{}", &response.response_body);
+
+    let body = response.response_body.clone();
+
+
+
+    stream.write(&mut body.as_bytes())?;
     stream.flush()?;
     Ok(())
 }
 
 fn serve(socket: SocketAddr) -> io::Result<()> {
-    let listener = TcpListener::bind(socket)?;
+    let listner = TcpListener::bind(socket)?;
     let mut counter = 0;
-    for stream in listener.incoming(){
-       match std::thread::spawn(|| handle_client(&mut stream?)).join(){
-            Ok(..) => {
+    for stream in listner.incoming() {
+        match std::thread::spawn(|| handle_client(&mut stream?)).join() {
+            Ok(_) => {
                 counter += 1;
-                println!("Connected stream... {}", counter);
+                println!("connected stream... {}", counter);
             }
-            Err(..) => continue,
-       };
+            Err(_) => continue,
+        };
     }
     Ok(())
 }
-
 fn main() -> io::Result<()> {
     let socket = create_socket();
     serve(socket)?;
